553fb0c02e5e59531bdd092597f14f80461bf87e
alexanderws <Alex.ws@icloud.com>
2019-08-27 16:58:51 +0200

    Updates

---
+++ precache-manifest.610819b8ffa22b3a7c90a6d79567ef4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904aae1c640b6bfde4a9cae21dc0f245",
+    "url": "/tilbudsviser/index.html"
+  },
+  {
+    "revision": "f5bacd930eda5d51440d",
+    "url": "/tilbudsviser/static/css/main.70147669.chunk.css"
+  },
+  {
+    "revision": "b501231876d794fecfc4",
+    "url": "/tilbudsviser/static/js/2.a652456a.chunk.js"
+  },
+  {
+    "revision": "f5bacd930eda5d51440d",
+    "url": "/tilbudsviser/static/js/main.bae8f55e.chunk.js"
+  },
+  {
+    "revision": "0944958e33ca249aae82",
+    "url": "/tilbudsviser/static/js/runtime~main.2d601f98.js"
+  }
+]);
